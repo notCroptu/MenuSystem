f2f26c5780ac1002cd6bdf028cca6fc01e24e637
notCroptu <martinsmariana1972@gmail.com>
2026-01-03 00:27:00 +0000

    fix point events requirment being audio source

--- Runtime/UIGadgets/PointEvents.cs
+++ Runtime/UIGadgets/PointEvents.cs
@@ -3,9 +3,9 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 /// <summary>
-/// Component must be in same game object as ui object to use hover sound
+/// Component must be in same game object as ui object to use
 /// </summary>
-[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(CanvasRenderer))]
 public class PointEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler , IPointerClickHandler// Maybe script like button audio that is used like raycasts instead for objects (maybe not needed because sound effect can be used in conjunction with custom interaction scripts to achieve that)
 {
     [SerializeField] private UnityAction _press;
